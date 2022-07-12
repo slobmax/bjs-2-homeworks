03f842bed8670502c79b916181437971644773bc
Maxim Slobozhanin <slob.max@gmail.com>
2022-07-13 02:02:23 +0400

    task 1 complete

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,16 @@
+"use strict"
+
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
+  let d = b**2-4*a*c;
+  if (d < 0) {
+    arr = [];
+  } else if (d === 0) {
+    arr = [(-b/(2*a))];
+  } else if (d > 0) {
+    arr = [((-b + Math.sqrt(d) )/(2*a)), (-b - Math.sqrt(d) )/(2*a)];
+  }
   return arr; // array
 }
 
