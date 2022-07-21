32d4cb13aed7950a0d2a6289b581d04c20977354
Maxim Slobozhanin <slob.max@gmail.com>
2022-07-21 12:38:39 +0400

    finalized

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,19 +1,13 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
+  
   // Ваш код
 
-  result = (arr1.length === arr2.length && arr1.every((element, index) => arr2[index] === element));
-
-  return result; // boolean
+  return (arr1.length === arr2.length && arr1.every((element, index) => arr2[index] === element)); // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
+  
   // Ваш код
 
-  resultArr = arr.filter(number => number > 0).filter(number => number % 3 === 0).map(number => number * 10);
-
-  return resultArr; // array
+  return arr.filter(number => number > 0).filter(number => number % 3 === 0).map(number => number * 10); // array
 }
