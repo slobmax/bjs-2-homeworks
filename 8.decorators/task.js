7e402a18aca2b777ae16cf18ba7c2e6cf4e055dd
Maxim Slobozhanin <slob.max@gmail.com>
2022-09-03 13:34:44 +0400

    tasks completed

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,43 @@
-function cachingDecoratorNew(func) {
-  // Ваш код
+function cachingDecoratorNew(func) {  
+  let cache = [];
+
+  function wrapper(...args) {    
+    const hash = args.join(',');
+
+    let objectInCache = cache.find((item) => item.hash === hash);
+
+    if(objectInCache){          
+      console.log("Из кэша: " + objectInCache.value);
+      return "Из кэша: " + objectInCache.value;      
+    }    
+
+    let result = func(...args);
+    cache.push({hash:hash, value:result});  
+
+    if (cache.length > 5) { 
+      cache.shift();  
+    }    
+
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+  wrapper.count = 0;
+  wrapper.allCount = 0;  
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+  setTimeout (() => {func(); wrapper.count++;}, 0)
+
+  function wrapper(...args) {
+    wrapper.allCount ++;
+    if(timeoutId) {            
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {func(...args); wrapper.count ++;}, delay);
+  }
+  return wrapper;
 }
